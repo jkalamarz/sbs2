393909018f4159a81190ce7f848ab42206c84c6b
jkalamarz <jkalamarz@gmail.com>
2019-03-13 22:44:22 -0700

    Initial Commit

---
+++ SBS2/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SBS2
+//
+//  Created by Jacek Kalamarz on 3/13/19.
+//  Copyright © 2019 Jacek Kalamarz. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ SBS2Tests/SBS2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SBS2Tests.swift
+//  SBS2Tests
+//
+//  Created by Jacek Kalamarz on 3/13/19.
+//  Copyright © 2019 Jacek Kalamarz. All rights reserved.
+//
+
+import XCTest
+@testable import SBS2
+
+class SBS2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
